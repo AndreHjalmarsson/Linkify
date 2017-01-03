43b78557413d257dfc20581fd85ee8c0e2be8930
AndreHjalmarsson <andre.hjalmarsson1@gmail.com>
2017-01-03 16:07:55 +0100

    Update how posts display
    Update how posts display

--- resources/lib/allPosts.php
+++ resources/lib/allPosts.php
@@ -1,9 +1,36 @@
 <?php
 
-$postContent = dbGet($connection, "SELECT * FROM posts INNER JOIN users ON users.id = posts.uid ORDER BY published DESC;");
+$postInfo = dbGet($connection, "SELECT * FROM posts INNER JOIN users ON users.id = posts.uid ORDER BY published DESC;");
 
-foreach($postContent as $post) {
-	echo $post["content"] ." ". $post["name"] . "<br>";
+
+foreach($postInfo as $post) {
+	$postContent = $post["content"];
+	$postTitle = $post["title"];
+	$postPublished = $post["published"];
+	$postUsername = $post["username"];
+	$postAvatar = $post["avatar"];
+	$uid = $post["uid"];
+	$postid = $post["postid"];
+
+	?>
+
+	<!DOCTYPE html>
+	<html>
+		<head>
+			<meta charset="utf-8">
+			<link rel="stylesheet" href="/resources/css/style.css">
+			<title></title>
+		</head>
+		<body>
+			<div class="placeholderAvatar"><img src="/resources/img/users/<?php echo $uid ?>/<?php echo $postAvatar; ?>" style="width: 100%; height: 100%;" alt=""></div>
+			<div class="postContent">
+				<h4><?= $postTitle; ?></h4> <br>
+				<a href="#"><?= $postContent; ?></a>
+			</div>
+			<br><br>
+		</body>
+	</html>
+
+<?php
+
 }
-
- ?>
