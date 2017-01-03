89ce970bb91abd9a7906966c075d6e47c1ea2bc0
AndreHjalmarsson <andre.hjalmarsson1@gmail.com>
2017-01-03 13:42:55 +0100

    Updated post function
    Made updated when displaying posts

--- resources/lib/allPosts.php
+++ resources/lib/allPosts.php
@@ -1,9 +1,9 @@
 <?php
 
-$posts = dbGet($connection, "SELECT * FROM posts ORDER BY published DESC;");
+$postContent = dbGet($connection, "SELECT * FROM posts INNER JOIN users ON users.id = posts.uid ORDER BY published DESC;");
 
-foreach($posts as $post) {
-	echo $post["content"] . "<br>";
+foreach($postContent as $post) {
+	echo $post["content"] ." ". $post["name"] . "<br>";
 }
 
  ?>
