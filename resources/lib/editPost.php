94de2f9447ed38febd2fa4e3934e482abc41ea55
AndreHjalmarsson <andre.hjalmarsson1@gmail.com>
2017-01-05 16:57:03 +0100

    update on editing posts
    update on editing posts

--- resources/lib/editPost.php
+++ resources/lib/editPost.php
@@ -1,12 +1,20 @@
 <?php
 require("functions.php");
 
+$url = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+$urlPostid = substr($url, -5, 1);
+
+$posts = dbGet($connection, "SELECT * FROM posts WHERE postid = '$urlPostid';");
+foreach ($posts as $post) {
+	$currentContent = $post["content"];
+	$currentTitle = $post["title"];
+}
 
 ?>
 
 <form method="POST" action="/resources/lib/insertEditPost.php">
 	<input type="hidden" name="editAction" value="editPost">
-	<input type="text" name="title" placeholder="Add title">
-   <textarea name="content" placeholder="Add your text here"></textarea>
+	<input type="text" name="title" value="<?= $currentTitle ?>">
+   <textarea name="content"><?= $currentContent ?></textarea>
    <button type="submit">Post</button>
 </form>
